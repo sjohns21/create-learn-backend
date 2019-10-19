9fc4b6adcff804215065dfb86da34b41b54bbee4
Steve Johnson <steve21johnson@gmail.com>
2019-10-19 15:21:29 -0700

    updateHour

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -40,14 +40,24 @@ export class TeacherController {
     return this.teacherService.getSingleProduct(prodId);
   }
 
-  @Patch(':id')
-  async updateProduct(
-    @Param('id') prodId: string,
-    @Body('title') prodTitle: string,
-    @Body('description') prodDesc: string,
-    @Body('price') prodPrice: number,
+  // @Patch(':id')
+  // async updateProduct(
+  //   @Param('id') prodId: string,
+  //   @Body('title') prodTitle: string,
+  //   @Body('description') prodDesc: string,
+  //   @Body('price') prodPrice: number,
+  // ) {
+  //   await this.teacherService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+  //   return null;
+  // }
+
+  @Patch('hour')
+  async updateHours(
+    @Body('teacherId') teacherId: string,
+    @Body('dayIndex') dayIndex: number,
+    @Body('hourIndex') hourIndex: number,
   ) {
-    await this.teacherService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    await this.teacherService.updateHour(teacherId, dayIndex, hourIndex);
     return null;
   }
 
