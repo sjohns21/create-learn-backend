6ba53271f27766f6a53a9b1b5714f407edf5601e
Steve Johnson <steve21johnson@gmail.com>
2019-10-20 10:48:50 -0700

    remove teacherId

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -15,48 +15,46 @@ export class TeacherController {
   constructor(private readonly teacherService: TeacherService) {
   }
 
-  @Post()
-  async addProduct(
-    @Body('title') prodTitle: string,
-    @Body('description') prodDesc: string,
-    @Body('price') prodPrice: number,
-  ) {
-    const generatedId = await this.teacherService.insertProduct(
-      prodTitle,
-      prodDesc,
-      prodPrice,
-    );
-    return { id: generatedId };
-  }
+  // @Post()
+  // async addProduct(
+  //   @Body('title') prodTitle: string,
+  //   @Body('description') prodDesc: string,
+  //   @Body('price') prodPrice: number,
+  // ) {
+  //   const generatedId = await this.teacherService.insertProduct(
+  //     prodTitle,
+  //     prodDesc,
+  //     prodPrice,
+  //   );
+  //   return { id: generatedId };
+  // }
 
   @Post('class')
   async addClass(
-    @Body('teacherId') teacherId: string,
     @Body('dayIndex') dayIndex: number,
     @Body('start') start: number,
     @Body('end') end: number,
   ) {
     return await this.teacherService.insertClass(
-      teacherId,
       dayIndex,
       start,
       end,
     );
   }
 
-  @Get()
-  async getAllProducts() {
-    const products = await this.teacherService.getProducts();
-    return products;
-  }
+  // @Get()
+  // async getAllProducts() {
+  //   const products = await this.teacherService.getProducts();
+  //   return products;
+  // }
 
   // @Get(':id')
   // getProduct(@Param('id') prodId: string) {
   //   return this.teacherService.getSingleProduct(prodId);
   // }
-  @Get(':id')
-  getTeacher(@Param('id') teacherId: string) {
-    return this.teacherService.findTeacher(teacherId);
+  @Get()
+  getTeacher() {
+    return this.teacherService.findTeacher();
   }
 
   // @Patch(':id')
@@ -76,12 +74,12 @@ export class TeacherController {
     @Body('dayIndex') dayIndex: number,
     @Body('hourIndex') hourIndex: number,
   ) {
-    return await this.teacherService.hourToggle(teacherId, dayIndex, hourIndex);
+    return await this.teacherService.hourToggle(dayIndex, hourIndex);
   }
 
-  @Delete(':id')
-  async removeProduct(@Param('id') prodId: string) {
-    await this.teacherService.deleteProduct(prodId);
-    return null;
-  }
+  // @Delete(':id')
+  // async removeProduct(@Param('id') prodId: string) {
+  //   await this.teacherService.deleteProduct(prodId);
+  //   return null;
+  // }
 }
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -11,21 +11,21 @@ export class TeacherService {
   ) {
   }
 
-  async insertProduct(title: string, desc: string, price: number) {
-    const newProduct = new this.teacherModel({
-      title,
-      description: desc,
-      price,
-    });
-    const result = await newProduct.save();
-    return result.id as string;
-  }
+  // async insertProduct(title: string, desc: string, price: number) {
+  //   const newProduct = new this.teacherModel({
+  //     title,
+  //     description: desc,
+  //     price,
+  //   });
+  //   const result = await newProduct.save();
+  //   return result.id as string;
+  // }
 
-  async insertClass(teacherId: string, dayIndex: number, start: number, end: number) {
+  async insertClass(dayIndex: number, start: number, end: number) {
     if (start > end) {
       throw new BadRequestException();
     }
-    const teacherBefore = await this.findTeacher(teacherId);
+    const teacherBefore = await this.findTeacher();
     const day = teacherBefore.hours[dayIndex];
 
     let windowIsAvailable = true;
@@ -43,10 +43,10 @@ export class TeacherService {
         day[hour] = 2;
       }
 
-      const updatedTeacher = await this.teacherModel.findByIdAndUpdate(
-        teacherId, { $set: { [`hours.${dayIndex}`]: day } }, { useFindAndModify: false },
+      const updatedTeacher = await this.teacherModel.findOneAndUpdate(
+        {}, { $set: { [`hours.${dayIndex}`]: day } }, { useFindAndModify: false },
       );
-      const teacherAfter = await this.findTeacher(teacherId);
+      const teacherAfter = await this.findTeacher();
       if (updatedTeacher && teacherAfter) {
         return { classAdded: true };
       } else {
@@ -58,52 +58,52 @@ export class TeacherService {
 
   }
 
-  async getProducts() {
-    const products = await this.teacherModel.find().exec();
-    return products.map(prod => ({
-      id: prod.id,
-      title: prod.title,
-      description: prod.description,
-      price: prod.price,
-    }));
-  }
+  //
+  // async getProducts() {
+  //   const products = await this.teacherModel.find().exec();
+  //   return products.map(prod => ({
+  //     id: prod.id,
+  //     title: prod.title,
+  //     description: prod.description,
+  //     price: prod.price,
+  //   }));
+  // }
 
-  async getSingleProduct(productId: string) {
-    const product = await this.findProduct(productId);
-    return {
-      id: product.id,
-      title: product.title,
-      description: product.description,
-      price: product.price,
-    };
-  }
+  // async getSingleProduct(productId: string) {
+  //   const product = await this.findProduct(productId);
+  //   return {
+  //     id: product.id,
+  //     title: product.title,
+  //     description: product.description,
+  //     price: product.price,
+  //   };
+  // }
 
-  async updateProduct(
-    productId: string,
-    title: string,
-    desc: string,
-    price: number,
-  ) {
-    const updatedProduct = await this.findProduct(productId);
-    if (title) {
-      updatedProduct.title = title;
-    }
-    if (desc) {
-      updatedProduct.description = desc;
-    }
-    if (price) {
-      updatedProduct.price = price;
-    }
-    // updatedProduct.save();
-  }
+  // async updateProduct(
+  //   productId: string,
+  //   title: string,
+  //   desc: string,
+  //   price: number,
+  // ) {
+  //   const updatedProduct = await this.findProduct(productId);
+  //   if (title) {
+  //     updatedProduct.title = title;
+  //   }
+  //   if (desc) {
+  //     updatedProduct.description = desc;
+  //   }
+  //   if (price) {
+  //     updatedProduct.price = price;
+  //   }
+  //   // updatedProduct.save();
+  // }
 
   async hourToggle(
-    teacherId: string,
     dayIndex: number,
     hourIndex: number,
   ) {
 
-    const teacherBefore = await this.findTeacher(teacherId);
+    const teacherBefore = await this.findTeacher();
     const prevState = teacherBefore.hours[dayIndex][hourIndex];
     let newState;
     if (prevState === 0) {
@@ -111,10 +111,10 @@ export class TeacherService {
     } else if (prevState === 1) {
       newState = 0;
     }
-    const updatedTeacher = await this.teacherModel.findByIdAndUpdate(
-      teacherId, { $set: { [`hours.${dayIndex}.${hourIndex}`]: newState } }, { useFindAndModify: false },
+    const updatedTeacher = await this.teacherModel.findOneAndUpdate(
+      {}, { $set: { [`hours.${dayIndex}.${hourIndex}`]: newState } }, { useFindAndModify: false },
     );
-    const teacherAfter = await this.findTeacher(teacherId);
+    const teacherAfter = await this.findTeacher();
     if (updatedTeacher && teacherAfter) {
       return { updated: true };
     } else {
@@ -122,30 +122,30 @@ export class TeacherService {
     }
   }
 
-  async deleteProduct(prodId: string) {
-    const result = await this.teacherModel.deleteOne({ _id: prodId }).exec();
-    if (result.n === 0) {
-      throw new NotFoundException('Could not find product.');
-    }
-  }
+  // async deleteProduct(prodId: string) {
+  //   const result = await this.teacherModel.deleteOne({ _id: prodId }).exec();
+  //   if (result.n === 0) {
+  //     throw new NotFoundException('Could not find product.');
+  //   }
+  // }
 
-  private async findProduct(id: string): Promise<Teacher> {
-    let product;
-    try {
-      product = await this.teacherModel.findById(id).exec();
-    } catch (error) {
-      throw new NotFoundException('Could not find product.');
-    }
-    if (!product) {
-      throw new NotFoundException('Could not find product.');
-    }
-    return product;
-  }
+  // private async findProduct(id: string): Promise<Teacher> {
+  //   let product;
+  //   try {
+  //     product = await this.teacherModel.findById(id).exec();
+  //   } catch (error) {
+  //     throw new NotFoundException('Could not find product.');
+  //   }
+  //   if (!product) {
+  //     throw new NotFoundException('Could not find product.');
+  //   }
+  //   return product;
+  // }
 
-  async findTeacher(id: string): Promise<Teacher> {
+  async findTeacher(): Promise<Teacher> {
     let teacher;
     try {
-      teacher = await this.teacherModel.findById(id).exec();
+      teacher = await this.teacherModel.findOne().exec();
     } catch (error) {
       throw new NotFoundException('Could not find teacher.');
     }
